76d66c0584e9a42950d0dcfa601b77affea71757
salihdemiir <salihdemir5335@gmail.com>
2026-05-12 13:18:37 +0300

    About update

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 
 import Home from "./pages/Home";
-import About from "./pages/Experience";
+import About from "./pages/About";
 import Experience from "./pages/Experience";
 import Projects from "./pages/Projects";
 import Skills from "./pages/Skills";
